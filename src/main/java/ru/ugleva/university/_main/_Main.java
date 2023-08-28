2c6831e0c60a31fd8d6e818069ada42b9b3c0f89
pourrpre <dhtvbvb@gmail.com>
2023-08-28 22:00:10 +0700

    В классе _Main удалены лишние отступы

--- src/main/java/ru/ugleva/university/_main/_Main.java
+++ src/main/java/ru/ugleva/university/_main/_Main.java
@@ -10,28 +10,20 @@ public class _Main {
         Student stud = new Student();
         University uni = new University();
         Teacher teacher = new Teacher();
-
         stud.setsName("Ivan Ivanov");
         uni.setuName("NSU");
         teacher.settName("Valentin Nikolaevich");
-
         stud.setUni(uni);
         stud.setTeacher(teacher);
-
         teacher.setUni(uni);
         teacher.setStud(stud);
-
         stud.callStud();
         teacher.callTeacher();
-
         stud.goToLesson();
         teacher.goToLesson();
-
         stud.writeOfHomework();
-
         stud.eat();
         teacher.eat();
-
         teacher.checkUpTest();
     }
 }
